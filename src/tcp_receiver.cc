80ffb5045f3f69b146d3fdb6da96956ec50062e1
GuoGuo614 <2251270969@qq.com>
2026-03-05 14:21:55 +0000

    Implement TCPReceiver's receive and send methods

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -6,13 +6,34 @@ using namespace std;
 void TCPReceiver::receive( TCPSenderMessage message )
 {
   // Your code here.
-  debug( "unimplemented receive() called" );
-  (void)message;
+  if ( message.RST ) {
+    reassembler_.reader().set_error();
+  }
+
+  if ( !ISN_flag ) {
+    if ( !message.SYN ) {
+      return;
+    }
+    ISN = message.seqno;
+    ISN_flag = true;
+  }
+
+  uint64_t checkpoint = reassembler_.writer().bytes_pushed() + 1;
+  uint64_t abs_seqno = message.seqno.unwrap( ISN, checkpoint );
+  uint64_t first_index = message.SYN ? 0 : abs_seqno - 1;
+
+  reassembler_.insert( first_index, std::move( message.payload ), message.FIN );
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
   // Your code here.
-  debug( "unimplemented send() called" );
-  return {};
+  std::optional<Wrap32> ack_opt {};
+  if ( ISN_flag ) {
+    ack_opt = ISN + reassembler_.writer().bytes_pushed() + 1 + ( reassembler_.writer().is_closed() ? 1 : 0 );
+  }
+
+  uint16_t win_size = min( reassembler_.writer().available_capacity(), static_cast<uint64_t>( UINT16_MAX ) );
+
+  return { .ackno = ack_opt, .window_size = win_size, .RST = reassembler_.reader().has_error() };
 }
--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -11,12 +11,12 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   uint32_t offset = raw_value_ - zero_point.raw_value_;
-  auto checkpoint_offset = static_cast<uint32_t>(checkpoint);
-  
+  auto checkpoint_offset = static_cast<uint32_t>( checkpoint );
+
   uint32_t diff = offset - checkpoint_offset;
-  
-  if (diff <= (1U << 31) || checkpoint + diff < (1ULL << 32)) {
+
+  if ( diff <= ( 1U << 31 ) || checkpoint + diff < ( 1ULL << 32 ) ) {
     return checkpoint + diff;
   }
-  return checkpoint - (1ULL << 32) + diff;
+  return checkpoint - ( 1ULL << 32 ) + diff;
 }
