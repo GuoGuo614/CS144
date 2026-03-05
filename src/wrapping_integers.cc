71377de5d66febd4fe3391f334c30868f32f8a85
GuoGuo614 <2251270969@qq.com>
2026-03-05 03:24:21 +0000

    Implement wrap and unwrap methods in Wrap32 class

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -5,14 +5,18 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  return zero_point + static_cast<uint32_t>( n );
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+  uint32_t offset = raw_value_ - zero_point.raw_value_;
+  auto checkpoint_offset = static_cast<uint32_t>(checkpoint);
+  
+  uint32_t diff = offset - checkpoint_offset;
+  
+  if (diff <= (1U << 31) || checkpoint + diff < (1ULL << 32)) {
+    return checkpoint + diff;
+  }
+  return checkpoint - (1ULL << 32) + diff;
 }
